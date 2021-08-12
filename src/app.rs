f01f869d73da432b229591f7901b38018d538bcd
Joel Nises <joel.nises@gmail.com>
2021-08-12 18:59:22 +0200

    clippy fix

--- src/app.rs
+++ src/app.rs
@@ -163,7 +163,7 @@ impl App for Pistolhot {
                     let mut prev = None;
                     let mut it = left_vis_buffer.iter().copied().rev();
                     it.nth(VIS_SIZE / 2 - 1);
-                    while let Some(value) = it.next() {
+                    for value in &mut it {
                         if let Some(prev) = prev {
                             if prev >= 0. && value < 0. {
                                 break;
